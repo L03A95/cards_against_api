52977b75aa67a413a1d32ad5961b98adddd89c61
Liam M. Perez Lupia <liamperezlupia53@gmail.com>
2023-11-27 18:39:48 -0300

    Added routes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const route = require('./routes/main')
 const app = express()
 const port = 3001
+
+app.use('/', route)
 
 app.listen(port, () => {
     console.log('se ha iniciado el servidor en el puerto ' + port)
