c2905bd8d6c8f4d72ff8dda6ff3cab268c3dd76e
Liam M. Perez Lupia <liamperezlupia53@gmail.com>
2023-12-01 14:43:26 -0300

    Improved user routes and controllers

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,17 @@ const User = require('../models/User')
 
 const signUp = async (user) => {
     try {
+        if(!user.username || !user.password || !user.email) {
+            throw new Error("Falta un parametro")
+        }
+        const existingUser = await User.findOne({ email: user.email });
+        if (existingUser) {
+            throw new Error("Ese email ya existe")
+        }
+        const existingUsername = await User.findOne({ username: user.username });
+        if (existingUsername) {
+            throw new Error("Ese nombre de usuario ya existe")
+        }
 
         const newUser = new User({...user})
         await newUser.save()
--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose')
 
 const UserSchema =  new mongoose.Schema({
-    username: String,
-    email: String,
-    password: String,
+    username: {
+        type: String,
+        required: true
+      },
+    email: {
+        type: String,
+        required: true
+      },
+    password: {
+        type: String,
+        required: true
+      },
     coins: {
         type: Number,
         default: 0
