2d95cea2e2963f2b7d913b4925f6d5d52aae36e8
laiamia5 <laiperez1020@gmail.com>
2024-02-22 18:19:54 -0300

    Made join to lobby logic

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -5,13 +5,26 @@ const {signUp} = require('../controllers/userController')
 let lobbys = []
 
 userRouter.post('/newLobby', (req,res) => {
-    lobbys.push({
-        id: lobbys.length,
-        admin: req.body.admin,
-        players: [req.body.admin]
-    })
-    console.log(lobbys)
-    res.status(200).json(lobbys[lobbys.length - 1])
+    try {
+        lobbys.push({
+            id: lobbys.length,
+            players: [req.body]
+        })
+        console.log(lobbys)
+        res.status(200).json(lobbys[lobbys.length - 1])
+    } catch (err) {
+        res.status(400).json(err.message)
+    }
+    
+})
+
+userRouter.get('/searchLobby', (req, res) => {
+    try {
+        let result = lobbys.find((lobby) => lobby.id = req.body.id);
+        res.status(200).json(result)
+    } catch (error) {
+        res.status(400).json(err.message)
+    }
 })
 
 module.exports = userRouter
