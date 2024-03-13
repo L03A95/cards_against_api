31f117a09615f961e46f1e3889f87a004d0779d6
Liam M. Perez Lupia <liamperezlupia53@gmail.com>
2024-03-13 00:54:42 -0300

    Improved lobby logic

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -4,7 +4,7 @@ const {signUp} = require('../controllers/userController')
 
 let lobbys = []
 
-userRouter.post('/newLobby', (req,res) => {
+userRouter.post('/newLobby', async (req,res) => {
     try {
         lobbys.push({
             id: lobbys.length,
@@ -16,14 +16,19 @@ userRouter.post('/newLobby', (req,res) => {
         res.status(400).json(err.message)
     }
     
+
 })
 
-userRouter.get('/searchLobby', (req, res) => {
+userRouter.post('/searchLobby/:id', async (req, res) => {
     try {
-        let result = lobbys.find((lobby) => lobby.id = req.body.id);
+        let result = await lobbys.find((lobby) => lobby.id == req.params.id);
+        if (!result) {
+            throw new Error("No se ha encontrado un Lobby con ese id")
+        }
+        lobbys[req.params.id].players.push({user: req.body.user, admin: false})
         res.status(200).json(result)
     } catch (error) {
-        res.status(400).json(err.message)
+        res.status(404).json(error.message)
     }
 })
 
